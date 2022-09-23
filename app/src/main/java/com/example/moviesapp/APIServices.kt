a039890e476da16a6b3f5b14cc2477b50a08961e
Adham Misallam <adham.misallam3@gmail.com>
2022-09-23 23:04:58 +0200

    Fourth Vesion

---
+++ app/src/main/java/com/example/moviesapp/APIServices.kt
@@ -0,0 +1,19 @@
+package com.example.moviesapp
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+const val API_KEY = "644a10f035ca4d11b1504511b3315fb8"
+
+interface APIServices {
+
+    @GET("movie/popular")
+    fun getPopularMovies(
+        @Query("api_key")
+        api_key: String = API_KEY,
+        @Query("page")
+        page: Int,
+    ): Call<MovieResponse>
+}
---
+++ app/src/main/java/com/example/moviesapp/MovieDetails.kt
@@ -0,0 +1,55 @@
+package com.example.moviesapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+
+class MovieDetails : AppCompatActivity() {
+    private lateinit var backdrop: ImageView
+    private lateinit var poster: ImageView
+    private lateinit var title: TextView
+    private lateinit var rating: RatingBar
+    private lateinit var releaseDate: TextView
+    private lateinit var overview: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+        backdrop = findViewById(R.id.movie_backdrop)
+        poster = findViewById(R.id.movie_poster)
+        title = findViewById(R.id.movie_title)
+        rating = findViewById(R.id.movie_rating)
+        releaseDate = findViewById(R.id.movie_release_date)
+        overview = findViewById(R.id.movie_overview)
+        val movieDetail = intent.getParcelableExtra<Movie>("MOVIE")
+        if (movieDetail != null) {
+            populateDetails(movieDetail)
+        } else {
+            finish()
+        }
+    }
+
+    private fun populateDetails(movieDetails: Movie) {
+        movieDetails.backgroundImage?.let { backdropPath ->
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w1280$backdropPath")
+                .transform(CenterCrop())
+                .into(backdrop)
+        }
+        movieDetails.poster.let { posterPath ->
+            Glide.with(this)
+                .load("https://image.tmdb.org/t/p/w342$posterPath")
+                .transform(CenterCrop())
+                .into(poster)
+        }
+        title.text = movieDetails.title
+        rating.rating = movieDetails.rate!!
+        releaseDate.text = movieDetails.release
+        overview.text = movieDetails.overview
+    }
+    }
