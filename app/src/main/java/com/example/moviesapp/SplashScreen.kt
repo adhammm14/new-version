9a1911df3b9e0f77d4626adb46f268c9d1af4b60
Adham Misallam <adham.misallam3@gmail.com>
2022-09-24 21:06:27 +0200

    6 Vesion

--- app/src/main/java/com/example/moviesapp/SplashScreen.kt
+++ app/src/main/java/com/example/moviesapp/SplashScreen.kt
@@ -13,11 +13,12 @@ import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
 
-    private val splashScreen = 5000
+    private val splashScreen = 4000
     private lateinit var topAnim: Animation
     private lateinit var bottomAnim: Animation
     private lateinit var imageView: ImageView
     private lateinit var label: TextView
+    private lateinit var label2: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,10 +36,12 @@ class SplashScreen : AppCompatActivity() {
         bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_anim)
         imageView = findViewById(R.id.splash_icon)
         label = findViewById(R.id.splash_label)
+        label2 = findViewById(R.id.label2)
 
         //Animation for splash screen
         imageView.animation = topAnim
         label.animation = bottomAnim
+        label2.animation = bottomAnim
 
 
         Handler().postDelayed({
